3cbf2aab8d45ea91fb80fb4a882c427b4ad51c47
Sudeep2704 <ds.sudeep04@gmail.com>
2025-10-13 02:51:30 +0530

    minor changes

---
+++ strings cpp/stringsearch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>     
+using namespace std;
+
+int main() {
+    
+
+   string str = "Hello World";
+   auto pos = str.find('W');
+    cout << pos << endl;
+    
+    return 0;
+}
---
+++ strings cpp/substring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>     
+using namespace std;
+
+int main() {
+    
+
+    string str = "Hello World";
+    string sub1 = str.substr(3,4);
+    cout << sub1;
+    
+    return 0;
+}
